e6f4b33bc8994e4163ee1d214688305ac5a87856
Timothy Kimble <timble417@gmail.com>
2021-09-07 15:09:53 -0600

    initialized and added env.js

--- Contractor.client/src/env.js
+++ Contractor.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-ci5b8144.us.auth0.com'
+export const audience = 'https://contractor.com'
+export const clientId = 'hGNYYdkrpU211PkVewC6QCXI55z2nZv6'
